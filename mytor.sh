28f2315a2f05b7d9d1b810b345ca03852e4c5589
Rodrigo Favarini <rodrigofavarini@gwsteam.com>
2020-04-28 13:54:23 +0100

    DebugLog

--- mytor.sh
+++ mytor.sh
@@ -6,7 +6,7 @@ if [ "$1" == "start" ]; then
     mkdir -p /tmp/tor/9001
     chmod 777 /tmp/tor/9001
     touch /tmp/tor/9001/torrc
-    echo "" >> /tmp/tor/9001/torrc
+    echo "" > /tmp/tor/9001/torrc
     echo "SocksPort 9001" >> /tmp/tor/9001/torrc
     echo "ControlPort 9091" >> /tmp/tor/9001/torrc
     echo "DNSPort 9053" >> /tmp/tor/9001/torrc
@@ -18,7 +18,7 @@ if [ "$1" == "start" ]; then
     mkdir -p /tmp/tor/9002
     chmod 777 /tmp/tor/9002
     touch /tmp/tor/9002/torrc
-    echo "" >> /tmp/tor/9002/torrc
+    echo "" > /tmp/tor/9002/torrc
     echo "SocksPort 9002" >> /tmp/tor/9002/torrc
     echo "ControlPort 9092" >> /tmp/tor/9002/torrc
     echo "DataDirectory /tmp/tor/9002" >> /tmp/tor/9002/torrc
@@ -27,7 +27,7 @@ if [ "$1" == "start" ]; then
     mkdir -p /tmp/tor/9003
     chmod 777 /tmp/tor/9003
     touch /tmp/tor/9003/torrc
-    echo "" >> /tmp/tor/9003/torrc
+    echo "" > /tmp/tor/9003/torrc
     echo "SocksPort 9003" >> /tmp/tor/9003/torrc
     echo "ControlPort 9093" >> /tmp/tor/9003/torrc
     echo "DataDirectory /tmp/tor/9003" >> /tmp/tor/9003/torrc
@@ -36,7 +36,7 @@ if [ "$1" == "start" ]; then
     mkdir -p /tmp/tor/9004
     chmod 777 /tmp/tor/9004
     touch /tmp/tor/9004/torrc
-    echo "" >> /tmp/tor/9004/torrc
+    echo "" > /tmp/tor/9004/torrc
     echo "SocksPort 9004" >> /tmp/tor/9004/torrc
     echo "ControlPort 9094" >> /tmp/tor/9004/torrc
     echo "DataDirectory /tmp/tor/9004" >> /tmp/tor/9004/torrc
@@ -45,7 +45,7 @@ if [ "$1" == "start" ]; then
     mkdir -p /tmp/tor/9005
     chmod 777 /tmp/tor/9005
     touch /tmp/tor/9005/torrc
-    echo "" >> /tmp/tor/9005/torrc
+    echo "" > /tmp/tor/9005/torrc
     echo "SocksPort 9005" >> /tmp/tor/9005/torrc
     echo "ControlPort 9095" >> /tmp/tor/9005/torrc
     echo "DataDirectory /tmp/tor/9005" >> /tmp/tor/9005/torrc

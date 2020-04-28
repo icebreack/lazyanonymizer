ef67d504ceb6773d8402dc138d41fb2e48f7e799
Rodrigo Favarini <rodrigofavarini@gwsteam.com>
2020-04-28 16:23:47 +0100

    add firewall rules

--- firewall.sh
+++ firewall.sh
@@ -1,39 +1,9 @@
-#!/bin/sh
- 
-### set variables
-#destinations you don't want routed through Tor
-_non_tor=""
- 
-#the UID that Tor runs as (varies from system to system)
-_tor_uid=`id -u debian-tor`
- 
-#Tor's TransPort
-_trans_port="15379"
- 
-### flush iptables
-iptables -F
-iptables -t nat -F
- 
-### set iptables *nat
-iptables -t nat -A OUTPUT -m owner --uid-owner $_tor_uid -j RETURN
-iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 53
- 
-#allow clearnet access for hosts in $_non_tor
-for _clearnet in $_non_tor 127.0.0.0/9 127.128.0.0/10; do
-   iptables -t nat -A OUTPUT -d $_clearnet -j RETURN
+iptables -t nat -A OUTPUT -m owner --uid-owner 137 -j RETURN
+
+for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
+    iptables -t nat -A OUTPUT -d $NET -j RETURN
 done
- 
-#redirect all other output to Tor's TransPort
-iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $_trans_port
- 
-### set iptables *filter
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
- 
-#allow clearnet access for hosts in $_non_tor
-for _clearnet in $_non_tor 127.0.0.0/8; do
-   iptables -A OUTPUT -d $_clearnet -j ACCEPT
-done
- 
-#allow only Tor output
-iptables -A OUTPUT -m owner --uid-owner $_tor_uid -j ACCEPT
-iptables -A OUTPUT -j REJECT
+
+iptables -t nat -A OUTPUT -p tcp -j DNAT --to-destination 127.0.0.1:15379
+iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 15379
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT   
